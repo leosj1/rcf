9a67d20d1bd6e60da513e410c44aa5d2c1fe3cd9
leosj1 <45917802+leosj1@users.noreply.github.com>
2019-03-02 21:56:19 +0000

    Add files via upload

---
+++ TOPICS.js
@@ -0,0 +1,225 @@
+const topics={
+    "type": "FeatureCollection",
+    "Topic 1": [
+        {
+            "name": "Topic 1",
+            "properties": {
+                "Blog distribution": "80%",
+                "Bloggers mentioned": "50",
+                "Posting Location": "100",
+                "Posts mentioned": "Nigeria"
+            },
+            "data": {
+                "word": 
+                    "Word5 Word2 Word1 Word8 Word5 Word6 Word1 Word3 Word2 Word4 Word10 Word2 Word9 Word1 Word9 Word2 Word3 Word7 Word1 Word2 Word3 Word1 Word1 Word2 Word1 Word1 Word1 Word2 Word5 Word1 Word1 Word2"
+                ,
+                "chord":[
+                    {
+                        "values": [6637, 5700, 4789, 2771],
+                        "text": "Samsung"
+                    },
+                    {
+                        "values": [7737, 2691, 2202, 7006],
+                        "text": "LG"
+                    },
+                    {
+                        "values": [8574, 9898, 4084, 1765],
+                        "text": "iPhone"
+                    },
+                    {
+                        "values": [857, 9876, 408, 1789],
+                        "text": "Sony Ericsson"
+                    },
+                    {
+                        "values": [5309, 1602, 8395, 2908],
+                        "text": "Nokia"
+                    }
+                ],
+                "scatter":[
+                    {
+                        "values": [
+                            [10, 90],
+                            [20, 15],
+                            [3, 21],
+                            [40, 30],
+                            [5, 40],
+                            [6, 590],
+                            [7, 60],
+                            [80, 75],
+                            [9, 81],
+                            [10, 99]
+                        ]
+                    },
+                    {
+                        "values": [
+                            [0.09, 3],
+                            [2.1, 103],
+                            [30.5, 25],
+                            [4.9, 35],
+                            [50.3, 41],
+                            [6.5, 57],
+                            [7.1, 61],
+                            [8.7, 70],
+                            [9.2, 82],
+                            [90.9, 95]
+                        ]
+                    },
+                    {
+                        "values": [
+                            [0.1, 9],
+                            [1.8, 21],
+                            [1.9, 29],
+                            [40.1, 33],
+                            [4.5, 39],
+                            [6.9, 51],
+                            [7.4, 64],
+                            [80.9, 70],
+                            [9, 750],
+                            [9.3, 93]
+                        ]
+                    },
+                    {
+                        "values": [
+                            [0.3, 11],
+                            [0.9, 15],
+                            [1.1, 24],
+                            [2.3, 209],
+                            [2.9, 30],
+                            [3.3, 35],
+                            [5.6, 67],
+                            [6.09, 700],
+                            [7.3, 71],
+                            [8.9, 90]
+                        ]
+                    },
+                    {
+                        "values": [
+                            [0.5, 5],
+                            [1.90, 50],
+                            [2.50, 10],
+                            [3.1, 30],
+                            [6.50, 45],
+                            [6.9, 74],
+                            [7.2, 50],
+                            [7.8, 56],
+                            [8, 61],
+                            [8.5, 71]
+                        ]
+                    }
+                ]
+            }
+        }
+       
+    ],
+    "Topic 2": [
+        {
+            "name": "Topic 1",
+            "properties": {
+                "Blog distribution": "80%",
+                "Bloggers mentioned": "50",
+                "Posting Location": "100",
+                "Posts mentioned": "Nigeria"
+            },
+            "data": {
+                "word": 
+                    "Word5 Word2 Word1 Word8 Word5 Word6 Word1 Word3 Word2 Word4 Word10 Word2 Word9 Word1 Word9 Word2 Word3 Word7 Word1 Word2 Word3 Word1 Word1 Word2 Word1 Word1 Word1 Word2 Word5 Word1 Word1 Word2"
+                ,
+                "chord":[
+                    {
+                        "values": [6637, 5700, 4789, 2771],
+                        "text": "Samsung"
+                    },
+                    {
+                        "values": [7737, 2691, 2202, 7006],
+                        "text": "LG"
+                    },
+                    {
+                        "values": [8574, 9898, 4084, 1765],
+                        "text": "iPhone"
+                    },
+                    {
+                        "values": [857, 9876, 408, 1789],
+                        "text": "Sony Ericsson"
+                    },
+                    {
+                        "values": [5309, 1602, 8395, 2908],
+                        "text": "Nokia"
+                    }
+                ],
+                "scatter":[
+                    {
+                        "values": [
+                            [1, 9],
+                            [2, 15],
+                            [3, 21],
+                            [4, 30],
+                            [5, 40],
+                            [6, 59],
+                            [7, 60],
+                            [8, 75],
+                            [9, 81],
+                            [10, 99]
+                        ]
+                    },
+                    {
+                        "values": [
+                            [0.9, 3],
+                            [2.1, 13],
+                            [3.5, 25],
+                            [4.9, 35],
+                            [5.3, 41],
+                            [6.5, 57],
+                            [7.1, 61],
+                            [8.7, 70],
+                            [9.2, 82],
+                            [9.9, 95]
+                        ]
+                    },
+                    {
+                        "values": [
+                            [0.1, 9],
+                            [1.8, 21],
+                            [1.9, 29],
+                            [4.1, 33],
+                            [4.5, 39],
+                            [6.9, 51],
+                            [7.4, 64],
+                            [8.9, 70],
+                            [9, 75],
+                            [9.3, 93]
+                        ]
+                    },
+                    {
+                        "values": [
+                            [0.3, 11],
+                            [0.9, 15],
+                            [1.1, 24],
+                            [2.3, 29],
+                            [2.9, 30],
+                            [3.3, 35],
+                            [5.6, 67],
+                            [6.9, 70],
+                            [7.3, 71],
+                            [8.9, 90]
+                        ]
+                    },
+                    {
+                        "values": [
+                            [0.5, 5],
+                            [1.9, 5],
+                            [2.5, 10],
+                            [3.1, 30],
+                            [6.5, 45],
+                            [6.9, 74],
+                            [7.2, 50],
+                            [7.8, 56],
+                            [8, 61],
+                            [8.5, 71]
+                        ]
+                    }
+                ]
+            }
+        }
+       
+    ]
+}
